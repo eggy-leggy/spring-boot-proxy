94822d1cd497d2f6ff1ce0d04e42428962459a71
ChunqiangFan <515235987@qq.com>
2019-09-03 17:54:35 +0800

    项目初始化

---
+++ proxy-oath2/src/main/java/com/eggy/Application.java
@@ -0,0 +1,26 @@
+package com.eggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ChunqiangFan
+ * @description: 主函数
+ * @create 2019-09-03 17:48
+ */
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+    @RequestMapping(value = "/hello")
+    public String hello() {
+        return "hello world";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
